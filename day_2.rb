d6a9ecbd770bd17dbd27067cd9866ac7bb6668b7
justinpaulson <justinapaulson@gmail.com>
2020-12-01 23:11:17 -0600

    Day 2

--- day_2.rb
+++ day_2.rb
@@ -1,2 +1,13 @@
-# first, second = File.readlines(ARGV[0]).map{|input| input.split(?,)}
-# inputs = File.read(ARGV[0]).chomp.split(", ")
+passes = File.readlines(ARGV[0]).map{|i| i.split ": "}
+t = 0
+l = 0
+passes.each do |p|
+  pass = p[1]
+  range, letter = p[0].split " "
+  low, high = range.split("-").map &:to_i
+  t+=1 if pass.count(letter) >= low && pass.count(letter) <= high
+  l+=1 if (pass[high-1]!=letter && pass[low-1]==letter) || (pass[high-1]==letter && pass[low-1]!=letter)
+end
+
+puts t
+puts l
