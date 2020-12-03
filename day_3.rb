668fea79c77c2486dd353b514010c5090491f40f
Justin Paulson <justinapaulson@gmail.com>
2020-12-03 10:30:24 -0600

    day 3 refactor !

--- day_3.rb
+++ day_3.rb
@@ -1,52 +1,18 @@
-grid = File.readlines(ARGV[0]).map(&:chars)
-with_viz=true
-t1,t2,t3,t4,t5=0,0,0,0,0
-current=0
-grid = grid[1..]
-current =0
-grid.each do |line|
-  current+=1
-  current=current-31 if current>=31
-  c = line[current]
-  t1+=1 if c=='#'
-  r = line[0..current-1]+[c=='#' ? 'X' : 'O']+line[current+1..]
-  puts r.join if with_viz
-end
-current =0
-grid.each do |line|
-  current+=3
-  current=current-31 if current>=31
-  c = line[current]
-  t2+=1 if c=='#'
-  r = line[0..current-1]+[c=='#' ? 'X' : 'O']+line[current+1..]
-  puts r.join if with_viz
-end
-current =0
-grid.each do |line|
-  current+=5
-  current=current-31 if current>=31
-  c = line[current]
-  t3+=1 if c=='#'
-  r = line[0..current-1]+[c=='#' ? 'X' : 'O']+line[current+1..]
-  puts r.join if with_viz
-end
-current =0
-grid.each do |line|
-  current+=7
-  current=current-31 if current>=31
-  c = line[current]
-  t4+=1 if c=='#'
-  r = line[0..current-1]+[c=='#' ? 'X' : 'O']+line[current+1..]
-  puts r.join if with_viz
-end
-grid.each_slice(2) do |_,line|
-  current+=1
-  current=current-31 if current>=31
-  c = line[current]
-  t5+=1 if c=='#'
-  r = line[0..current-1]+[c=='#' ? 'X' : 'O']+line[current+1..]
-  puts r.join if with_viz
+grid = File.readlines(ARGV[0]).map(&:chop).map(&:chars)
+with_viz=false
+l=grid[0].length
+g=1
+s=[[1,1],[1,3],[1,5],[1,7],[2,1]]
+s.each do |rise,run|
+  t,y,x=0,0,0
+  while y<grid.length-rise
+    y+=rise
+    x+=run
+    t+=1 if grid[y][x%l]=="#"
+    puts ((x%l>0 ? (grid[y][0..x%l-1]) : [])+[grid[y][x%l]=="#" ? "X" : "O"]+grid[y][x%l+1..]).join if with_viz
+  end
+  puts "Slope (#{rise},#{run}): #{t}"
+  g*=t
 end
 
-puts t1
-puts t1*t2*t3*t4*t5
+puts g
