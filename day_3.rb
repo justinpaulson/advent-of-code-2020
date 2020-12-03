9bf67504a055abb20f9410e831d84d70895acdf6
Justin Paulson <justinapaulson@gmail.com>
2020-12-03 11:55:36 -0600

    change variable name

--- day_3.rb
+++ day_3.rb
@@ -1,5 +1,5 @@
 grid = File.readlines(ARGV[0]).map(&:chop).map(&:chars)
-with_viz=false
+viz=false
 l=grid[0].length
 g=1
 s=[[1,1],[1,3],[1,5],[1,7],[2,1]]
@@ -9,7 +9,7 @@ s.each do |rise,run|
     y+=rise
     x+=run
     t+=1 if grid[y][x%l]=="#"
-    puts ((x%l>0 ? (grid[y][0..x%l-1]) : [])+[grid[y][x%l]=="#" ? "X" : "O"]+grid[y][x%l+1..]).join if with_viz
+    puts ((x%l>0 ? (grid[y][0..x%l-1]) : [])+[grid[y][x%l]=="#" ? "X" : "O"]+grid[y][x%l+1..]).join if viz
   end
   puts "Slope (#{rise},#{run}): #{t}"
   g*=t
